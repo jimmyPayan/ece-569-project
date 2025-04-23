9c6c328602302946b204b05b95ee5426857ce313
Jimmy Edward Payan <jimmypayan@i16n0.ocelote.hpc.arizona.edu>
2025-04-23 15:16:53 -0700

    doesnt track object properly. went to a 2D grid with 2 for loops instead of 3D grid with 1 for loop

--- final_project/KCF-CUDA/src/n4complexity.cu
+++ final_project/KCF-CUDA/src/n4complexity.cu
@@ -9,12 +9,11 @@ __global__ void kernel_n4(int sizeY, int sizeX, int k, int height, int width, in
 	// Use thread IDs as iterators, same names as joaofaro to keep me sane while debugging
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
-	int ii = blockIdx.z * blockDim.z + threadIdx.z;
+	//int ii = blockIdx.z * blockDim.z + threadIdx.z;
 	
 	int d;
-	//debug
-	//printf("thread %d %d %d says hello\n", i, j, ii);
 if (i < sizeY && j < sizeX) {
+	for(int ii = 0; ii < k; ii++) { /////////////////////////////////////
 	for(int jj = 0; jj < k; jj++)
           {
             if ((i * k + ii > 0) && 
@@ -22,70 +21,79 @@ if (i < sizeY && j < sizeX) {
                 (j * k + jj > 0) && 
                 (j * k + jj < width  - 1))
             {
-              d = (k * i + ii) * width + (j * k + jj);
-             d_map[ i * stringSize + j * numFeatures + d_alfa[d * 2    ]] += 
-                d_r[d] * d_w[ii * 2] * d_w[jj * 2];
-	     // atomicAdd(&d_map[ i * stringSize + j * numFeatures + d_alfa[d * 2    ]],
-             //      d_r[d] * d_w[ii * 2] * d_w[jj * 2]);
+            d = (k * i + ii) * width + (j * k + jj);
+           //  d_map[ i * stringSize + j * numFeatures + d_alfa[d * 2    ]] += 
+           //     d_r[d] * d_w[ii * 2] * d_w[jj * 2];
+	      atomicAdd(&d_map[ i * stringSize + j * numFeatures + d_alfa[d * 2    ]],
+                   d_r[d] * d_w[ii * 2] * d_w[jj * 2]);
              
-	      d_map[ i * stringSize + j * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR] += 
-                 d_r[d] * d_w[ii * 2] * d_w[jj * 2];
-             // atomicAdd(&d_map[ i * stringSize + j * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR],
-	     //      d_r[d] * d_w[ii * 2] * d_w[jj * 2]);
+	   //   d_map[ i * stringSize + j * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR] += 
+           //      d_r[d] * d_w[ii * 2] * d_w[jj * 2];
+              atomicAdd(&d_map[ i * stringSize + j * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR],
+	           d_r[d] * d_w[ii * 2] * d_w[jj * 2]);
 	      
 	      if ((i + d_nearest[ii] >= 0) && 
                   (i + d_nearest[ii] <= sizeY - 1))
               {
-                d_map[(i + d_nearest[ii]) * stringSize + j * numFeatures + d_alfa[d * 2    ]             ] += 
-                d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 ];
-	      //atomicAdd(&d_map[(i + d_nearest[ii]) * stringSize + j * numFeatures + d_alfa[d * 2    ]],
-	      //    d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 ]);
+           //     d_map[(i + d_nearest[ii]) * stringSize + j * numFeatures + d_alfa[d * 2    ]             ] += 
+           //     d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 ];
+	      atomicAdd(&d_map[(i + d_nearest[ii]) * stringSize + j * numFeatures + d_alfa[d * 2    ]],
+	          d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 ]);
 
-                d_map[(i + d_nearest[ii]) * stringSize + j * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR] += 
-                  d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 ];
-	     // atomicAdd(&d_map[(i + d_nearest[ii]) * stringSize + j * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR],
-	     //       d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 ]);
+           //     d_map[(i + d_nearest[ii]) * stringSize + j * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR] += 
+           //       d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 ];
+	      atomicAdd(&d_map[(i + d_nearest[ii]) * stringSize + j * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR],
+	            d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 ]);
 
               }
               if ((j + d_nearest[jj] >= 0) && 
                   (j + d_nearest[jj] <= sizeX - 1))
               {
-                d_map[i * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2    ]             ] += 
-                  d_r[d] * d_w[ii * 2] * d_w[jj * 2 + 1];
-	     // atomicAdd(&d_map[i * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2    ]             ],
-	     //       d_r[d] * d_w[ii * 2] * d_w[jj * 2 + 1]);          
+            //    d_map[i * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2    ]             ] += 
+            //      d_r[d] * d_w[ii * 2] * d_w[jj * 2 + 1];
+	      atomicAdd(&d_map[i * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2    ]             ],
+	            d_r[d] * d_w[ii * 2] * d_w[jj * 2 + 1]);          
 
-	        d_map[i * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR] += 
-                  d_r[d] * d_w[ii * 2] * d_w[jj * 2 + 1];
-	     // atomicAdd(&d_map[i * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR],
-	     //       d_r[d] * d_w[ii * 2] * d_w[jj * 2 + 1]);
+	    //    d_map[i * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR] += 
+            //      d_r[d] * d_w[ii * 2] * d_w[jj * 2 + 1];
+	      atomicAdd(&d_map[i * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR],
+	            d_r[d] * d_w[ii * 2] * d_w[jj * 2 + 1]);
               }
               if ((i + d_nearest[ii] >= 0) && 
                   (i + d_nearest[ii] <= sizeY - 1) && 
                   (j + d_nearest[jj] >= 0) && 
                   (j + d_nearest[jj] <= sizeX - 1))
               {
-                d_map[(i + d_nearest[ii]) * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2    ]             ] += 
-                  d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 + 1];
-	      //atomicAdd(&d_map[(i + d_nearest[ii]) * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2    ]],
-	      //      d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 + 1]);
+            //    d_map[(i + d_nearest[ii]) * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2    ]             ] += 
+            //      d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 + 1];
+	      atomicAdd(&d_map[(i + d_nearest[ii]) * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2    ]],
+	            d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 + 1]);
 
-                d_map[(i + d_nearest[ii]) * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR] += 
-                  d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 + 1];
-	      //atomicAdd(&d_map[(i + d_nearest[ii]) * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR],
-	      //      d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 + 1]);
+            //    d_map[(i + d_nearest[ii]) * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR] += 
+            //      d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 + 1];
+	      atomicAdd(&d_map[(i + d_nearest[ii]) * stringSize + (j + d_nearest[jj]) * numFeatures + d_alfa[d * 2 + 1] + NUM_SECTOR],
+	            d_r[d] * d_w[ii * 2 + 1] * d_w[jj * 2 + 1]);
               }
             } // if()
 } // for(int jj = 0; jj < k; jj++)
+} // for(int ii = 0; ii < k; ii++)
 } // if (i < sizeY && j < sizeX)
 }
 
 
 void featureGPU(int sizeY, int sizeX, int k, int height, int width, int numFeatures, float *map, int stringSize, int *alfa, float *r, float *w, int *nearest){
+	printf("Running kernel_n4.\n");
     float *d_map, *d_r, *d_w;
     int *d_alfa, *d_nearest;
-
+// Commented out cudaMalloc()'s are passed by value
+    //cudaMalloc((void**) &sizeY, sizeof(int));
+    //cudaMalloc((void**) &sizeX, sizeof(int));
+    //cudaMalloc((void**) &k,     sizeof(int));
+    //cudaMalloc((void**) &height, sizeof(int);
+    //cudaMalloc((void**) &width, sizeof(int));
+    //cudaMalloc((void**) &numFeatures, sizeof(int));
     cudaMalloc((void**) &d_map, sizeof(float) * (sizeX * sizeY * numFeatures));
+    //cudaMalloc((void**) &stringSize, sizeof(int));
     cudaMalloc((void**) &d_alfa, sizeof(int) * (width * height * 2));
     cudaMalloc((void**) &d_r, sizeof(float) * (width * height));
     cudaMalloc((void**) &d_w, sizeof(float) * (k * 2));
@@ -98,8 +106,8 @@ void featureGPU(int sizeY, int sizeX, int k, int height, int width, int numFeatu
     cudaMemcpy(d_nearest, nearest, sizeof(int) * k, cudaMemcpyHostToDevice); 
 
     // Total number of threads needed: sizeY * sizeX * k... max value of k is 4, which occurs during HOG. 1024 / 4 = 256, sqrt(256) = 16.
-    const dim3 threadsPerBlock(16, 16, k);
-    const dim3 blocksPerGrid(ceil((float)sizeY / 16), ceil((float)sizeX / 16));
+    const dim3 threadsPerBlock(32,32);
+    const dim3 blocksPerGrid(ceil((float)sizeY / 32), ceil((float)sizeX / 32));
 
     kernel_n4<<<blocksPerGrid,threadsPerBlock>>>(sizeY, sizeX, k, height, width, numFeatures, map, stringSize, alfa, r, w, nearest);
 
