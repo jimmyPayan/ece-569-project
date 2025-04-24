744a15bb961322289a22449e82989917ec376a3a
rraad02 <rraad@arizona.edu>
2025-04-24 11:55:13 -0700

    Update runtracker.cpp

--- final_project/KCF-CUDA/src/runtracker.cpp
+++ final_project/KCF-CUDA/src/runtracker.cpp
@@ -16,7 +16,7 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char* argv[]){
-  cout << "ECE569 C Implementation: No CUDA optimizations." << endl;
+  cout << "ECE569 v0 normalizeAndTruncate Cuda optimization." << endl;
   cout << "Operating at " << fixed << CLOCKS_PER_SEC << " ticks per second (important for time.h)." << endl;
   float tick_rate = CLOCKS_PER_SEC / 1000;
 
