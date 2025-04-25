7cbede03985d53664b5f70d504926da02ab66a3c
rraad02 <rraad@arizona.edu>
2025-04-25 15:49:05 -0700

    Update normalizeAndTruncate.cuh

--- final_project/KCF-CUDA/src/normalizeAndTruncate.cuh
+++ final_project/KCF-CUDA/src/normalizeAndTruncate.cuh
@@ -7,6 +7,6 @@
 #include "fhog.hpp"  // for CvLSVMFeatureMapCaskade, NUM_SECTOR, etc.
 
 // Host‐callable entry point
-int normalizeAndTruncateNaive(CvLSVMFeatureMapCaskade* map, const float alfa);
+int normalizeAndTruncateGPU(CvLSVMFeatureMapCaskade* map, const float alfa);
 
 #endif // NORMALIZE_AND_TRUNCATE_CUH
