234584f62be25e14d467db4b750ac44ae4b89e03
Jimmy Edward Payan <jimmypayan@i16n19.ocelote.hpc.arizona.edu>
2025-05-02 14:17:54 -0700

    cleaning up CUDA version for submission

---
+++ final_project/buildKCF/CMakeFiles/KCF.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/u20/jimmypayan/ece-569-project/final_project/KCF-CUDA/src/gaussianCorrelation.cu" "CMakeFiles/KCF.dir/src/gaussianCorrelation.cu.o" "gcc" "CMakeFiles/KCF.dir/src/gaussianCorrelation.cu.o.d"
+  "/home/u20/jimmypayan/ece-569-project/final_project/KCF-CUDA/src/n4complexity.cu" "CMakeFiles/KCF.dir/src/n4complexity.cu.o" "gcc" "CMakeFiles/KCF.dir/src/n4complexity.cu.o.d"
+  "/home/u20/jimmypayan/ece-569-project/final_project/KCF-CUDA/src/normalizeAndTruncate.cu" "CMakeFiles/KCF.dir/src/normalizeAndTruncate.cu.o" "gcc" "CMakeFiles/KCF.dir/src/normalizeAndTruncate.cu.o.d"
+  "/home/u20/jimmypayan/ece-569-project/final_project/KCF-CUDA/src/fhog.cpp" "CMakeFiles/KCF.dir/src/fhog.cpp.o" "gcc" "CMakeFiles/KCF.dir/src/fhog.cpp.o.d"
+  "/home/u20/jimmypayan/ece-569-project/final_project/KCF-CUDA/src/kcftracker.cpp" "CMakeFiles/KCF.dir/src/kcftracker.cpp.o" "gcc" "CMakeFiles/KCF.dir/src/kcftracker.cpp.o.d"
+  "/home/u20/jimmypayan/ece-569-project/final_project/KCF-CUDA/src/runtracker.cpp" "CMakeFiles/KCF.dir/src/runtracker.cpp.o" "gcc" "CMakeFiles/KCF.dir/src/runtracker.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ final_project/buildKCF/CMakeFiles/KCF.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KCF.dir/src/fhog.cpp.o"
+  "CMakeFiles/KCF.dir/src/fhog.cpp.o.d"
+  "CMakeFiles/KCF.dir/src/gaussianCorrelation.cu.o"
+  "CMakeFiles/KCF.dir/src/gaussianCorrelation.cu.o.d"
+  "CMakeFiles/KCF.dir/src/kcftracker.cpp.o"
+  "CMakeFiles/KCF.dir/src/kcftracker.cpp.o.d"
+  "CMakeFiles/KCF.dir/src/n4complexity.cu.o"
+  "CMakeFiles/KCF.dir/src/n4complexity.cu.o.d"
+  "CMakeFiles/KCF.dir/src/normalizeAndTruncate.cu.o"
+  "CMakeFiles/KCF.dir/src/normalizeAndTruncate.cu.o.d"
+  "CMakeFiles/KCF.dir/src/runtracker.cpp.o"
+  "CMakeFiles/KCF.dir/src/runtracker.cpp.o.d"
+  "KCF"
+  "KCF.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/KCF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
